c74f00a67ebbc7c46e3c0c1e8174464697a07606
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-07-17 19:05:46 +0530

    append routes with /api/

--- index.js
+++ index.js
@@ -6,23 +6,34 @@ const pool = require("./dbConnect")
 // app.use(cors)
 // app.use(express.json())
 
-app.get("/test", async(req, res)=>{
+app.get("/api/test", async(req, res)=>{
     let msg = [{success: "Successful API request"}, {failure: "Failed API request!"}]
     try{
-        console.log("Hello")
-        return res.sendStatus(200)
+        return res.status(200).send("Hello")
     } catch(err){
-        console.log("olleH")
         return res.sendStatus(500)
     }
 })
 
-app.get("/acc", async(req, res)=> {
+app.get("/api/acc", async(req, res)=> {
     try{
         const result = await pool.query("SELECT * FROM ACCOUNTS")
         return res.json(result.rows)
     } catch(err) {
-        console.error(err.message)
+        return res.sendStatus(500)
+    }
+})
+
+
+app.get("/api/add-user", async(req, res)=> {
+    try{
+        const result = await pool.query("SELECT * FROM user_role_management WHERE uid=(%s) AND role='admin'");
+        if(result.length <= 0){
+            return res.sendStatus(404);
+        } else {
+        //     Insert Statement for admin to tenant
+        }
+    } catch (err){
         return res.sendStatus(500)
     }
 })
